e5c215da745dba2767354f0d6f432c37ea95ec45
Ben Wilson <benjamin.m.wilson@gmail.com>
2019-04-11 16:24:38 -0700

    added tslint

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,12 @@
-import uWS from 'uWebSockets.js';
+import uWS from "uWebSockets.js";
 const port = 9001;
 
 const app = uWS.App({ })
-    .get('/*', (res, req) => {
-        res.writeHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ message: 'Hello World!' }));
+    .get("/*", (res, req) => {
+      res.writeHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ message: "Hello World!" }));
     })
     .listen(port, () => {
-        console.log('Listening to port ' + port);
+        // tslint:disable-next-line:no-console
+      console.log("Listening to port " + port);
     });
